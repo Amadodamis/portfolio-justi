0800106be1ed8ca991620c617044bab6cbdc2814
unknown <amadodamis@gmail.com>
2025-08-25 11:55:29 -0300

    estructura+react routes

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Página Principal</h1>
+      <p>Aquí se mostrará el portafolio.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h1>Acceso de Administrador</h1>
+      <p>Aquí irá el formulario de login.</p>
+    </div>
+  );
+}
+
+export default Login;
